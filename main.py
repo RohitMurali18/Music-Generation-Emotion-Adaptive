41e1871994f8d60a76cc022680c989ae7752e31a
RohitMurali18 <124786787+RohitMurali18@users.noreply.github.com>
2025-04-30 00:43:22 -0500

    mapping

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from emotion_analysis import inference,data_preprocessing,EATS
 
-prompt = "i am walking down a road and i see a rainbow and it is sunny. i love life. then suddenly it turns dark and cloudy. it starts raining and i start crying."
+prompt = "i am walking down a road and i see a rainbow and it is sunny. i love life."
 # predictions = inference.predict_all_labels(prompt)
 # print(predictions)
-segmentation = data_preprocessing.segment_text(prompt)
-n = len(segmentation)
-allpredictions = []
-for i in range(0,n):
-    predictions = inference.predict(segmentation[i])
-    allpredictions.append(predictions)
-
+# segmentation = data_preprocessing.segment_text(prompt)
+# n = len(segmentation)
+# allpredictions = []
+# for i in range(0,n):
+#     predictions = inference.predict(segmentation[i])
+#     allpredictions.append(predictions)
+allpredictions = inference.predict(prompt)
 mapping = EATS.get_music_params(allpredictions)
 print(mapping)
